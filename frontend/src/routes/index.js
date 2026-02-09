243ac2d94d27219e6f188acc4e9356281fbe5f2a
lucas-hood <mostafa.bsh@gmail.com>
2026-02-09 20:21:45 +0800

    added rating component and single product page

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,11 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
 import RootLayout from "../layouts/RootLayout";
 import HomeScreen from "../screens/HomeScreen";
+import ProductScreen from "../screens/ProductScreen";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     Component: RootLayout,
-    children: [{ index: true, Component: HomeScreen }],
+    children: [
+      { index: true, Component: HomeScreen },
+      {
+        path: "/product",
+        children: [{ path: ":id", Component: ProductScreen }],
+      },
+    ],
   },
 ]);
